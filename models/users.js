d34a3b9ff4f81edc36caac5cae7db680fd7e0776
Nathan1133 <94014154+Nathan1133@users.noreply.github.com>
2022-04-03 17:58:32 -0400

    added proper regex for email

--- models/users.js
+++ models/users.js
@@ -11,8 +11,8 @@ const UserSchema = new Schema({
         type: String,
         required: true,
         unique: true,
-        // use REGEX to validate email
-        match: [/.+@.+\..+/]
+        // REGEX used to validate email
+        match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/]
     },
     thoughts: [{
         type: Schema.Types.ObjectId,
